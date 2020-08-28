eeee0647c5a098be3fe595abc15f813963ea28ea
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-27 22:30:27 -0400

    Programming Exercise 1-10

---
+++ Comments.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Comments {
+    public static void main(String[] args){
+        // Program comments are nonexecuting statements you add to a file for documentation.
+        /*
+                Program comments are nonexecuting statements you add to a file for documentation.
+         */
+        /** Program comments are nonexecuting statements you add to a file for documentation. */
+
+        System.out.println("Program comments are nonexecuting statements you add to a file for documentation.");
+    }
+}
