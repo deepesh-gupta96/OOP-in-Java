db3a2af41bd3e76d14c1b4a0bfadb88e6ec46c81
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-27 22:31:52 -0400

    Programming Exercise 1-12

---
+++ BurmaShave.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.JOptionPane;
+public class BurmaShave {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null," If you dislike");
+        JOptionPane.showMessageDialog(null," Big traffic fines\n");
+        JOptionPane.showMessageDialog(null," Slow down");
+        JOptionPane.showMessageDialog(null," 'Till you'");
+        JOptionPane.showMessageDialog(null,"Can read these signs");
+        JOptionPane.showMessageDialog(null," Burma-Shave");
+
+    }
+}
+
