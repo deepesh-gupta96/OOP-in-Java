320e1ace8892c6ef2a9eb4cb23e5b3826006af3a
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-27 22:28:40 -0400

    Programming Exercise 1-8

---
+++ TableAndChairs.java
@@ -0,0 +1,13 @@
+/**Write, compile, and test a class that displays the pattern shown in Figure 1-25.**/
+package com.company;
+
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
+    }
+}
