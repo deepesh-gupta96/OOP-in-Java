498cfb410ae8736125aa27a8ed4fd00237804273
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-27 22:31:27 -0400

    Programming Exercise 1-11

---
+++ CommentsDialog.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.JOptionPane;
+public class CommentsDialog {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Program comments are nonexecuting statements \n" +
+                "         you add to a file for documentation.");
+    }
+
+}
