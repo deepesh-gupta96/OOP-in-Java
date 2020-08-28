37f34731497b250a66e24fae62367514d326d284
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-27 22:27:56 -0400

    Programming 1-7

---
+++ MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args){
+        System.out.println("Quote:  zindagi lambi nahi badi honi chahiye…..");
+        System.out.println("Movie: Anand");
+        System.out.println("Actor: Rajesh Khanna");
+        System.out.println("Year: 1971");
+
+    }
+}
