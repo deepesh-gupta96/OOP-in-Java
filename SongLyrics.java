a64fbf22a4418af2bee9031b1fe119a4fbe627cb
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-27 22:27:24 -0400

    Programming 1-6

---
+++ SongLyrics.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class SongLyrics {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Tu jo roo tha toh kaun hansega\n" +
+                "Tu jo chhoota toh kaun rahega\n" +
+                "Tu chup hai toh yeh darr lag ta hai\n" +
+                "Apna mujh koko ab kaun kahega\n" +
+                "Tu hi wajah...\n" +
+                "Tere bina bewajah bekar hoon main\n" +
+                "Tera yaar hoon main\n" +
+                "Tera yaarhoon main");
+    }
+}
