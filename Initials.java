2f1c8023ff3aaa8aa789fcf54249321736bcd726
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-29 15:20:25 -0400

    Programming Exercise 2-10

---
+++ Initials.java
@@ -0,0 +1,11 @@
+/**Write a program that declares variables named init1, init2, and init3, to hold your three initials.
+ * Display the three initials with a period following each one, as in J.M.F.*/
+package com.company;
+
+public class Initials {
+    public static void main(String[] args) {
+        char init1 = 'J', init2 = 'M', init3 = 'F';
+        System.out.println(init1+"."+init2+"."+init3+".");
+    }
+}
+
